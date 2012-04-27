3b203e2bcfee074ca6d61f60b1977a46f97b921e
Phil Cohen <github@phlippers.net>
2012-04-27 16:28:10 -0700

    fix copy-paste metadata typos :|

--- metadata.rb
+++ metadata.rb
@@ -1,12 +1,12 @@
-name             "traceroute"
+name             "personality"
 maintainer       "Phil Cohen"
 maintainer_email "github@phlippers.net"
 license          "MIT"
-description      "Installs traceroute"
+description      "Installs personality"
 long_description IO.read(File.join(File.dirname(__FILE__), "README.md"))
 version          "1.0.0"
 
-recipe "traceroute", "Installs traceroute"
+recipe "personality", "Setup server personality"
 
 %w[debian ubuntu].each do |os|
   supports os
